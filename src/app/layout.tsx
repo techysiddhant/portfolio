beb4ff5f0d68d9e51e8c3ab67ad205bebc73cd55
techysiddhant <sidjain8000@gmail.com>
2024-10-23 14:26:49 +0530

    add favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/png" href="/favicon-48x48.png" sizes="48x48" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body className={twMerge(inter.variable, calistoga.variable, "bg-gray-900 text-white antialiased font-sans")}>
         <Toaster richColors position="top-right" />
         {children}</body>
